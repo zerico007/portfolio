755c15dcbb174b67a0a63416cff93505f0218d4e
Bavin Edwards <65621465+zerico007@users.noreply.github.com>
2024-07-04 19:24:25 -0500

    nav & fotter positionng

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,22 +5,28 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <p>Created by Bavin Edwards &copy; {new Date().getFullYear()}</p>
-      <a href="https://github.com/zerico007" target="_blank" rel="noreferrer">
-        <GitHub size={18} color="var(--primary-red)" />
-      </a>
-      <a
-        href="https://www.linkedin.com/in/bavin-edwards-777b23180/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <Linkedin size={18} color="var(--primary-red)" />
-      </a>
-      <a href="mailto:bavin_edwards@live.com" target="_blank" rel="noreferrer">
-        <Inbox size={18} color="var(--primary-red)" />
-      </a>
-      <a href="https://x.com/BavinEdwards" target="_blank" rel="noreferrer">
-        <Twitter size={18} color="var(--primary-red)" />
-      </a>
+      <div className={styles.socials}>
+        <a href="https://github.com/zerico007" target="_blank" rel="noreferrer">
+          <GitHub size={18} color="var(--primary-red)" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/bavin-edwards-777b23180/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Linkedin size={18} color="var(--primary-red)" />
+        </a>
+        <a
+          href="mailto:bavin_edwards@live.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Inbox size={18} color="var(--primary-red)" />
+        </a>
+        <a href="https://x.com/BavinEdwards" target="_blank" rel="noreferrer">
+          <Twitter size={18} color="var(--primary-red)" />
+        </a>
+      </div>
     </footer>
   );
 }
